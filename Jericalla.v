f2a77c52ff8dd74db90106e6a1987bac8db7db37
Alexk-AG <pepechuy5002@gmail.com>
2025-03-12 10:55:50 -0600

    Update Jericalla.v

--- Jericalla.v
+++ Jericalla.v
@@ -1,9 +1,13 @@
-module Jericalla_tb;
+module Jericalla (
+    input [16:0] instruction,  // Entrada de 17 bits que contiene las instrucciones
+    output reg [31:0] result,  // Salida de 32 bits
+    output reg zf              // Salida del flag zf
+);
+
     reg en;
     reg [3:0] dir1, dir2, dirW;
     reg [3:0] op;
-    wire [31:0] data1, data2, result, ram_out;
-	wire zf;
+    wire [31:0] data1, data2, ram_out;
     
     ROM rom_inst (
         .dir1R2(dir1),
