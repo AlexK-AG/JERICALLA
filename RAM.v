b4111a38b0b06e67539875d75f74eb23256d012b
Alexk-AG <pepechuy5002@gmail.com>
2025-03-18 23:51:53 -0600

    Update RAM.v

--- RAM.v
+++ RAM.v
@@ -4,9 +4,16 @@ module RAM (
 	input En,
 	input [3:0] dirR,
 	input [31:0] dataIn,
-	output reg [31:0] DS
+	output reg[31:0] DS
 );
 
 reg [31:0]memRam[0:15];
+
+always @(*) begin
+        if (En) begin
+            memRam[dirR] = dataIn; // Escribe en la memoria si En está activo
+        end
+        DS = memRam[dirR];         // Lee de la memoria (siempre)
+    end
 
 endmodule
